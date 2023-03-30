9cc39ccc90a0edf4cdd246375b5f11eff42b8318
bup-fogoo <94633380@qq.com>
2023-03-30 22:28:18 +0800

    update

---
+++ common/Database.go
@@ -0,0 +1,57 @@
+package common
+
+import (
+	"awesomeProject0511/model"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/spf13/viper"
+	"log"
+	"net/url"
+)
+
+func InitDB() *gorm.DB {
+	driverName := viper.GetString("datasource.driverName")
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
+	loc := viper.GetString("datasource.loc")
+	var args = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true&loc=%s",
+		username,
+		password,
+		host,
+		port,
+		database,
+		charset,
+		url.QueryEscape(loc))
+	db, err := gorm.Open(driverName, args)
+	if err != nil {
+		log.Fatalf("Database connection failed")
+	}
+
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+			log.Fatalf(fmt.Sprintf("Database transaction failed: %v", r))
+		} else {
+			tx.Commit()
+		}
+	}()
+
+	//tx.AutoMigrate(&model.User{}, &model.Video{}, &model.Category{}, &model.Comment{}, &model.CommentRelation{})
+	if err := tx.AutoMigrate(&model.User{}, &model.Video{}, &model.Category{}, &model.Comment{}, &model.CommentRelation{}, &model.Role{}, &model.Permission{}, &model.RolePermission{}).Error; err != nil {
+		log.Fatalf("无法迁移表格：" + err.Error())
+	}
+	tx.Model(&model.User{}).AddUniqueIndex("idx_user_username", "username")
+	tx.Model(&model.Video{}).AddIndex("idx_video_title", "title")
+	tx.Model(&model.Video{}).AddForeignKey("category_id", "categories(id)", "CASCADE", "CASCADE")
+	tx.Model(&model.Video{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
+	tx.Model(&model.Comment{}).AddIndex("idx_comment_user_video", "user_id", "video_id")
+	tx.Model(&model.CommentRelation{}).AddIndex("idx_comment_ancestor_descendant", "ancestor_id", "descendant_id")
+
+	return db
+}
---
+++ model/UserModel.go
@@ -0,0 +1,77 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type User struct {
+	gorm.Model
+	Username    string       `gorm:"not null;uniqueIndex"`
+	Password    string       `gorm:"not null"`
+	Nickname    string       `gorm:"not null"`
+	Email       string       `gorm:"not null;index"`
+	AvatarUrl   string       `gorm:"not null"`
+	Roles       []Role       `gorm:"many2many:user_roles;"`
+	Permissions []Permission `gorm:"many2many:user_permissions;"`
+	Videos      []Video
+	Likes       []Video `gorm:"many2many:user_likes;association_foreignkey:id;foreignkey:id"`
+	Followees   []*User `gorm:"many2many:user_followees;ForeignKey:FollowerID"`
+	Followers   []*User `gorm:"many2many:user_followers;ForeignKey:FolloweeID"`
+	Comments    []Comment
+}
+
+type Video struct {
+	gorm.Model
+	Title       string `gorm:"not null"`
+	Description string `gorm:"not null"`
+	Url         string `gorm:"not null"`
+	CoverUrl    string `gorm:"not null"`
+	Views       int
+	Likes       int
+	Collections int
+	Quality     string `gorm:"not null"`
+	CategoryID  uint
+	Category    Category `gorm:"foreignKey:CategoryID"`
+	UserID      uint
+	User        User `gorm:"foreignKey:UserID"`
+	Comments    []Comment
+	Tags        string `gorm:"not null"`
+}
+
+type Category struct {
+	gorm.Model
+	Name        string `gorm:"not null"`
+	Description string `gorm:"not null"`
+	Videos      []Video
+}
+
+type Comment struct {
+	gorm.Model
+	Content  string `gorm:"not null"`
+	UserID   uint
+	VideoID  uint
+	ParentID *uint
+	Children []Comment `gorm:"foreignkey:ParentID"`
+}
+
+type CommentRelation struct {
+	gorm.Model
+	AncestorID   uint //祖先节点ID
+	DescendantID uint //后代节点ID
+	Level        uint //评论层级
+}
+
+type Permission struct {
+	ID          uint   `gorm:"primary_key"`
+	Name        string `gorm:"unique"`
+	Description string
+	Roles       []Role `gorm:"many2many:role_permission;"`
+}
+
+type Role struct {
+	ID          uint   `gorm:"primary_key"`
+	Name        string `gorm:"unique"`
+	Description string
+	Permissions []Permission `gorm:"many2many:role_permission;"`
+}
